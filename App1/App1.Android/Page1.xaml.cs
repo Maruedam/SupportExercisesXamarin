1f9bd8780ddbe267f5af5d67c8fa2437565bcd6a
[maruedam] <[miuelangelruedamuriel@gmail.com]>
2019-06-04 03:23:04 +0200

    ejercicio 1 terminado con exito

---
+++ App1/App1.Android/Page1.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1.Droid
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await ShowMessage("mensaje iportante", "Al fin me ha salido", "menos mal", async () =>
+            {
+                await ShowMessage("OK ha sido presionado", "confirmamos que funcina, equipo", "suuuuu", null);
+            });
+        }
+        public async Task ShowMessage(string message, string title, string buttonText, Action afterHideCallback)
+        {
+
+            await DisplayAlert(title, message, buttonText);
+
+            afterHideCallback?.Invoke();
+        }
+    }
+}
---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await ShowMessage("mensaje iportante", "Al fin me ha salido", "menos mal", async () =>
+            {
+                await ShowMessage("OK ha sido presionado", "confirmamos que funcina, equipo", "suuuuu", null);
+            });
+        }
+        public async Task ShowMessage(string message, string title, string buttonText, Action afterHideCallback)
+        {
+
+            await DisplayAlert(title, message, buttonText);
+
+            afterHideCallback?.Invoke();
+        }
+    }
+}
